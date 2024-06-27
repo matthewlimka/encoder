4dd1bf5c06942424584f33c277d55f97b48a784b
Matthew.Lim <matthew.lim@fdmgroup.com>
2024-06-27 15:48:00 +0800

    Updated to reflect changes in Encoder and new ModifiedCaesarCipher class

--- src/main/java/com/matthewlim/EncoderApp/App.java
+++ src/main/java/com/matthewlim/EncoderApp/App.java
@@ -3,8 +3,16 @@ package com.matthewlim.EncoderApp;
 public class App {
 	
     public static void main(String[] args) {
-    	Encoder encoder = Encoder.getInstance();
-    	encoder.setOffset('Z'); // Exception will be thrown if character is not A-Z, 0-9 or special characters ()*+,-./
+    	char[] referenceChars = {
+    		    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+    		    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+    		    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(', ')', '*', 
+    		    '+', ',', '-', '.', '/'
+    		};
+    	
+    	char offsetChar = 'Z';
+    	Cipher cipher = new ModifiedCaesarCipher(offsetChar, referenceChars);
+    	Encoder encoder = new Encoder(cipher);
     	
     	String plainText = "HELLO WORLD";
     	String encodedText = encoder.encode(plainText);
